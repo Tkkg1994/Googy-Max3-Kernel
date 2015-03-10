a082fc4c78cc965778f4f2d0c858346dbd5eb2a4
Tkkg1994 <luca.grifo@outlook.com>
2015-03-10 22:59:21 +0100

    builenv

--- build_kernel_cm12.sh
+++ build_kernel_cm12.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 export KERNELDIR=`readlink -f .`
-export RAMFS_SOURCE="/home/googy/Anas/Ramdisks/ramfs_cm12"
+export RAMFS_SOURCE="/root/Ramdisks/GT-I9505-CM12"
 export PARENT_DIR=`readlink -f ..`
 export USE_SEC_FIPS_MODE=true
-export CROSS_COMPILE=/home/googy/Anas/linaro_a15_4.9.3-2015.02/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=/Kernel_Folder/Toolchain_4.9.3-2015.02_a15/bin/arm-cortex_a15-linux-gnueabihf-
 
 # if [ "${1}" != "" ];then
 #  export KERNELDIR=`readlink -f ${1}`
 # fi
 
-RAMFS_TMP="/home/googy/Anas/tmp_cm12/ramfs_cm12"
+RAMFS_TMP="/root/Ramdisks/GT-I9505-TW/tmp_tw/ramfs"
 
-VER="\"-GoogyMax3_CM12-v$1\""
-cp -f /home/googy/Anas/Googy-Max3-Kernel/Kernel/arch/arm/configs/0googymax3_cm12_defconfig /home/googy/Anas/Googy-Max3-Kernel/0googymax3_cm12_defconfig
-sed "s#^CONFIG_LOCALVERSION=.*#CONFIG_LOCALVERSION=$VER#" /home/googy/Anas/Googy-Max3-Kernel/0googymax3_cm12_defconfig > /home/googy/Anas/Googy-Max3-Kernel/Kernel/arch/arm/configs/0googymax3_cm12_defconfig
+VER="\"-Hulk-Kernel_CM12-V1$1\""
+cp -f /root/Hulk-Kernel/arch/arm/configs/0hulk_cm12_defconfig /root/Hulk-Kernel/0hulk_TW_defconfig
+sed "s#^CONFIG_LOCALVERSION=.*#CONFIG_LOCALVERSION=$VER#" /root/Hulk-Kernel/0hulk_cm12_defconfig > /root/Hulk-Kernel/arch/arm/configs/0hulk_cm12_defconfig
 
 if [ "${2}" = "x" ];then
  make mrproper || exit 1
@@ -26,7 +26,7 @@ find -name '*.ko' -exec rm -rf {} \;
 # fi
 
 # 
-make 0googymax3_cm12_defconfig VARIANT_DEFCONFIG=jf_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig || exit 1
+make 0hulk_cm12_defconfig VARIANT_DEFCONFIG=jf_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig DEBUG_DEFCONFIG=jfuserdebug_defconfig || exit 1
 
 . $KERNELDIR/.config
 
@@ -52,10 +52,10 @@ find $RAMFS_TMP -name EMPTY_DIRECTORY -exec rm -rf {} \;
 #remove mercurial repository
 rm -rf $RAMFS_TMP/.hg
 #copy modules into ramfs
-mkdir -p /home/googy/Anas/Googy-Max3-Kernel/GT-I9505_GoogyMax3_CM12.CWM/system/lib/modules
-rm -rf /home/googy/Anas/Googy-Max3-Kernel/GT-I9505_GoogyMax3_CM12.CWM/system/lib/modules/*
-find -name '*.ko' -exec cp -av {} /home/googy/Anas/Googy-Max3-Kernel/GT-I9505_GoogyMax3_CM12.CWM/system/lib/modules/ \;
-${CROSS_COMPILE}strip --strip-unneeded /home/googy/Anas/Googy-Max3-Kernel/GT-I9505_GoogyMax3_CM12.CWM/system/lib/modules/*
+mkdir -p /home/linux/Downloads/ROM_Folder/ArchiKitchen-master/PROJECT_HULK/system/lib/modules
+rm -rf /home/linux/Downloads/ROM_Folder/ArchiKitchen-master/PROJECT_HULK/system/lib/modules/*
+find -name '*.ko' -exec cp -av {} /home/linux/Downloads/ROM_Folder/ArchiKitchen-master/PROJECT_HULK/system/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded /home/linux/Downloads/ROM_Folder/ArchiKitchen-master/PROJECT_HULK/system/lib/modules/*
 
 cd $RAMFS_TMP
 find | fakeroot cpio -H newc -o > $RAMFS_TMP.cpio 2>/dev/null
@@ -66,12 +66,10 @@ cd -
 ./mkbootimg64 --kernel $KERNELDIR/arch/arm/boot/zImage --ramdisk $RAMFS_TMP.cpio.gz --cmdline "console=null androidboot.hardware=qcom user_debug=31 androidboot.selinux=permissive" -o $KERNELDIR/boot.img --base "0x80200000" --ramdiskaddr "0x82200000"
 #	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 androidboot.selinux=permissive' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 --output $PACKAGEDIR/boot.img
 
-cd /home/googy/Anas/Googy-Max3-Kernel
-mv -f -v /home/googy/Anas/Googy-Max3-Kernel/Kernel/boot.img /home/googy/Anas/Googy-Max3-Kernel/GT-I9505_GoogyMax3_CM12.CWM/boot.img
-cd /home/googy/Anas/Googy-Max3-Kernel/GT-I9505_GoogyMax3_CM12.CWM
-zip --symlinks -r ../GoogyMax3_CM12-Kernel_${1}_CWM.zip .
-
-adb push /home/googy/Anas/Googy-Max3-Kernel/GoogyMax3_CM12-Kernel_${1}_CWM.zip /storage/sdcard1/GoogyMax3_CM12-Kernel_${1}_CWM.zip || adb push /home/googy/Anas/Googy-Max3-Kernel/GoogyMax3_CM12-Kernel_${1}_CWM.zip /storage/sdcard0/GoogyMax3_CM12-Kernel_${1}_CWM.zip
+cd /home/linux/Downloads/ROM_Folder/ArchiKitchen-master/PROJECT_HULK
+mv -f -v /root/Hulk-Kernel/boot.img /home/linux/Downloads/ROM_Folder/ArchiKitchen-master/PROJECT_HULK/boot.img
+cd /home/linux/Downloads/ROM_Folder/ArchiKitchen-master/PROJECT_HULK
+zip -r ../Hulk-Kernel_TW-${1}_CWM.zip .
 
 # adb push /home/googy/Anas/Googy-Max3-Kernel/GoogyMax3_CM12-Kernel_${1}_CWM.zip /storage/sdcard1/update-gmax3.zip
 # 
