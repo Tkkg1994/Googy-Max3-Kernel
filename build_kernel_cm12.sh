351c6ebbb95ab90ff8d0470425e983a41670adeb
Luca Grifo <luca.grifo@outlook.com>
2015-04-23 22:06:06 +0000

    buildenv: fix forgotten make

--- build_kernel_cm12.sh
+++ build_kernel_cm12.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {
 	make mrproper
-	0hulk_cm12_defconfig VARIANT_DEFCONFIG=jf_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig DEBUG_DEFCONFIG=jfuserdebug_defconfig
+	make 0hulk_cm12_defconfig VARIANT_DEFCONFIG=jf_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig DEBUG_DEFCONFIG=jfuserdebug_defconfig
         make -j3
 }
