e6fbdfc9be8be9a025bcf08d0ca947a81c5ff93d
Tkkg1994 <luca.grifo@outlook.com>
2015-04-29 19:53:58 +0200

    Small fixes

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -171,6 +171,9 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
+// If build is user build and enforce option is not set, selinux is always permissive
+	new_value = 0;
+
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
